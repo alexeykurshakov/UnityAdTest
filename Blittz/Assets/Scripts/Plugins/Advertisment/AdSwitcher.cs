a1e6d4b72f2c62a4843abd93f5b6b746c11e0bf2
akurshakov <alexey.kurshakov@gmail.com>
2014-11-23 14:21:55 +0300

    add ad switcher, update scene

---
+++ Blittz/Assets/Scripts/Plugins/Advertisment/AdSwitcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdSwitcher : MonoBehaviour 
+{
+	private void OnGUI()
+	{
+		if (GUILayout.Button (AdsManager.Instance.IsShown ? "Hide" : "Show"))
+		{
+			if (AdsManager.Instance.IsShown)
+			{
+				AdsManager.Instance.HideBanner();
+			}
+			else
+			{
+				AdsManager.Instance.ShowBanner();
+			}
+		}
+	}
+}
